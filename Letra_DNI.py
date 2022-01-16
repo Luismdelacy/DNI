e11737b36710f1ed013274bb156e4f2511500d2b
Luis Mª <97833037+Luismdelacy@users.noreply.github.com>
2022-01-16 13:09:49 +0100

    Update Letra_DNI.py

--- Letra_DNI.py
+++ Letra_DNI.py
@@ -10,3 +10,5 @@ print(f"Su DNI es: {numero}{letra}")
 Registro = open("Registro_DNI.txt","a+",encoding="UTF-8")
 Registro.write("El DNI introducido es: " + str(numero) + str(letra) + "\n")
 Registro.close()
+
+# Luismdelacy
