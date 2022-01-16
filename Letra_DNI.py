1a4da88e024a8ddf4596200e68662a2fac16de4d
Luis Mª <97833037+Luismdelacy@users.noreply.github.com>
2022-01-16 13:08:11 +0100

    Add files via upload

---
+++ Letra_DNI.py
@@ -0,0 +1,12 @@
+# Variables
+numero = int(input("Introduzca los numeros de su DNI: "))
+letrasDNI = "TRWAGMYFPDXBNJZSQVHLCKE"
+
+# Código
+numletra = numero % 23
+letra = letrasDNI[numletra]
+print(f"Su DNI es: {numero}{letra}")
+
+Registro = open("Registro_DNI.txt","a+",encoding="UTF-8")
+Registro.write("El DNI introducido es: " + str(numero) + str(letra) + "\n")
+Registro.close()
